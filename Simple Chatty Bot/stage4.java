faff603dbb0505f08f533e1ba4bbc6a360a2fced
HaiNguyen267 <88306303+HaiNguyen267@users.noreply.github.com>
2021-08-28 13:28:09 +0700

    Create stage4.java

---
+++ Simple Chatty Bot/stage4.java
@@ -0,0 +1,47 @@
+package bot;
+
+import java.util.Scanner;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+        System.out.println("Please, remind me your name.");
+
+        String name = scanner.nextLine();
+
+        System.out.println("What a great name you have, " + name + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
+
+         int r1 = scanner.nextInt();
+        int r2 = scanner.nextInt();
+        int r3 = scanner.nextInt();
+
+
+        int temp = 0;
+        while (temp % 3 != r1 || temp % 5 != r2 || temp % 7 != r3) {
+            temp ++;
+        }
+        int yourAge = temp;
+
+        System.out.println("Your age is " + yourAge + "; that's a good time to start programming!");
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+
+        int number = scanner.nextInt();
+        int time = 0;
+        while (time <= number) {
+            System.out.println(time + "!");
+            time++;
+        }
+
+
+        System.out.println("Completed, have a nice day!");
+    }
+}
