e08b5a676d51f29f22f9a39c6506515f86037b20
HaiNguyen267 <88306303+HaiNguyen267@users.noreply.github.com>
2021-11-06 10:05:42 +0700

    Update stage1.java

--- Battleship/stage1.java
+++ Battleship/stage1.java
@@ -1,4 +1,6 @@
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -25,15 +27,13 @@ public class Main {
 
         int shipNum = 0;
 
-        BattleShip[] ships = {BattleShip.CARRIER, BattleShip.BATTLESHIP, BattleShip.CRUISER,
-                BattleShip.SUBMARINE, BattleShip.DESTROYER};
+        BattleShip[] ships = {BattleShip.CARRIER, BattleShip.BATTLESHIP, BattleShip.CRUISER, BattleShip.SUBMARINE, BattleShip.DESTROYER};
 
 
         String[][] grids = new String[11][11];
         setup(grids);
 
-        String instruction = String.format("\nEnter the coordinates of the %s (%d cells)\n",
-                ships[shipNum].name, ships[shipNum].size);
+        String instruction = String.format("\nEnter the coordinates of the %s (%d cells)\n", ships[shipNum].name, ships[shipNum].size);
 
         printGrids(grids);
         while (shipNum < 5) {
@@ -48,9 +48,8 @@ public class Main {
                     setLocation(ships[shipNum], location, grids);
                     printGrids(grids);
                     shipNum++;
-                    instruction = String.format("\nEnter the coordinates of the %s (%d cells): \n",
-                            ships[shipNum].name, ships[shipNum].size);
-                    System.out.println("shipNum: "+ shipNum);
+                    instruction = String.format("\nEnter the coordinates of the %s (%d cells): \n", ships[shipNum].name, ships[shipNum].size);
+                    //System.out.println("shipNum: "+ shipNum);
                 }
             } catch (WrongLocation e) {
                 instruction = e.getMessage();
@@ -89,7 +88,14 @@ public class Main {
         int toY = (int)location[1].charAt(0) - 64;
         int toX = Integer.parseInt(location[1].substring(1));
         //System.out.println(Arrays.toString(new int[]{fromY, fromX, toY, toX}));
-        return new int[]{fromY, fromX, toY, toX};
+        //System.out.println(Arrays.toString(new int[]{Math.min(fromY, toY), Math.min(fromX, toX), Math.max(fromY, toY), Math.max(fromX, toX)}));
+        
+        
+        // the cordinates of the beginning cells of the ship will always be <= the coordinates of the ending cells
+        /* the ship will be placed from left to right (if it is horizontal)
+            or top to bottom (if it is vertical)
+        */
+        return new int[]{Math.min(fromY, toY), Math.min(fromX, toX), Math.max(fromY, toY), Math.max(fromX, toX)}; 
     }
 
     public static boolean checkLocation(BattleShip ship, int[] location) throws WrongLocation {
@@ -104,13 +110,13 @@ public class Main {
             }
         }
 
-        int xDifference = Math.abs(location[3] - location[1]);
-        int yDifference = Math.abs(location[2] - location[0]);
+        int xDifference = location[3] - location[1];
+        int yDifference = location[2] - location[0];
 
         if (xDifference == 0 && yDifference + 1 == ship.size) {
-            correctLocation = true;
+            correctLocation = true; // when the ship is placed vertically
         } else if (xDifference + 1== ship.size && yDifference == 0) {
-            correctLocation = true;
+            correctLocation = true; // when the ship is placed horizontally
         }
         if (correctLocation) {
             return true;
@@ -121,86 +127,109 @@ public class Main {
     }
 
     public static void setLocation(BattleShip ship, int[] location, String[][] grids) throws WrongLocation {
-        boolean horizontal = location[2] - location[0] == 0;
+        boolean horizontal = location[0] == location[2]; // check the ship is horizontally or vertically placed
 
-        for (int i = 0; i < grids.length; i++) {
+        int fromY = location[0];
+        int fromX = location[1];
+        int toY = location[2];
+        int toX = location[3];
 
-            for (int j = 0; j < grids[i].length; j++) {
 
-                if (horizontal) {
+        boolean hasNorthSide = fromY > 1; // along with horizontal
+        boolean hasSouthSide = toY < 10; // along with horizontal
+        boolean hasWestSide = fromX > 1;// along with vertical
+        boolean hasEastSide = toX < 10;// along with vertical
 
-                    if (i == location[0] && j >= Math.min(location[1], location[3])  && j <= Math.max(location[1], location[3]) ) {
+        if (horizontal) {
+            
+            /*
+                           North close
+                           | | | | | |
+             west close <- O O O O O O -> east close
+                           | | | | | |
+                           South close
+                            
+             */
 
-                        //check close
-                        if (Math.min(location[1], location[3]) > 1 && j == Math.min(location[1], location[3])) {
-                            if (grids[i][j - 1].equals("O ")) {
-                                throw new WrongLocation("Error! You placed it too close to another one. Try again:");
-                            }
-                        } // check head1
-                        if (Math.max(location[1], location[3]) < 10 && j == Math.max(location[1], location[3])) {
-                            if (grids[i][j + 1].equals("O ")) {
-                                throw new WrongLocation("Error! You placed it too close to another one. Try again:");
-                            }
-                        } // check head2
+            
+            int yUnchanged = fromY; // the Y coordinate of the cells is the same (fromY = toY)
+            
+            // check west side close to another ship or not 
+            if (hasWestSide && grids[yUnchanged][fromX-1].equals("O ")) {
+                throw new WrongLocation("Error! You place the ship too close to another one. Try again: ");
+            }
+            
+            // check east side close to another ship or not 
+            if (hasEastSide && grids[yUnchanged][toX + 1].equals("O ")) {
+                throw new WrongLocation("Error! You place the ship too close to another one. Try again: ");
+            }
 
-                        if (i < 10) {
-                            if (grids[i + 1][j].equals("O ")) {
-                                throw new WrongLocation("Error! You placed it too close to another one. Try again:");
-                            }
-                        } // check side1 close to other ship
-                        if (i > 1) {
-                            if (grids[i - 1][j].equals("O ")) {
-                                throw new WrongLocation("Error! You placed it too close to another one. Try again:");
-                            }
-                        } // check side2 close to other ship 
+            for (int j = fromX; j <= toX ; j++) {
+                // check the ship cross another ship or not, if not place the ship
+                if (grids[yUnchanged][j].equals("O ")) {
+                    throw new WrongLocation("Error! You place the ship cross another one. Try again: ");
+                } else {
+                    grids[yUnchanged][j] = "O ";
+                }
+                
+                // check the north side along the ship whether it close to another ship or not
+                if (hasNorthSide && grids[yUnchanged - 1][j].equals("O ")) {
+                    throw new WrongLocation("Error! You place the ship too close to another one. Try again: ");
+                }
+                
+                // check the south side along the ship whether it close to another ship or not
+                if (hasSouthSide && grids[yUnchanged + 1][j].equals("O ")) {
+                    throw new WrongLocation("Error! You place the ship too close to another one. Try again: ");
+                }
+            }
+        } else {
+            
+             /*
+                           North close
+                               |
+                 west close <- O -> east close
+                 west close <- O -> east close
+                 west close <- O -> east close
+                 west close <- O -> east close
+                 west close <- O -> east close
+                               |
+                           South close
+                            
+             */
+            
+            int xUnchanged = fromX; // the X coordinate of the cells is the same (fromX = toX)
+            // check north side close to another ship or not 
+            if (hasNorthSide && grids[fromY - 1][xUnchanged].equals("O ")) {
+                throw new WrongLocation("Error! You place the ship too close to another one. Try again: ");
+            }
+            
+            // check south side close to another ship or no
+            if (hasSouthSide && grids[toY + 1][xUnchanged].equals("O ")) {
+                throw new WrongLocation("Error! You place the ship too close to another one. Try again: ");
+            } 
 
-                        if (grids[i][j].equals("O ")) {
-                            throw new WrongLocation("Error! A ship cannot cross other");
-                        } else {
-                            grids[i][j] = "O ";
-                        }
-                    }
-
-                } else  {
-
-                    if  (j == location[1] && i >= Math.min(location[0], location[2])  && i <= Math.max(location[0], location[2])) {
-                        //check close
-                        if (Math.min(location[0], location[2]) > 1 && i == Math.min(location[0], location[2])) {
-                            if (grids[i - 1][j].equals("O ")) {
-                                //System.out.println("head123");
-                                throw new WrongLocation("Error! You placed it too close to another one. Try again:");
-                            }
-                        } // check head1
-                        if (Math.max(location[0], location[2]) < 10 && i == Math.max(location[0], location[2])) {
-                            if (grids[i + 1][j].equals("O ")) {
-                                //System.out.println("head456");
-                                throw new WrongLocation("Error! You placed it too close to another one. Try again:");
-                            }
-                        } // check head2
-
-                        if (j < 10) {
-                            if (grids[i][j + 1].equals("O ")) {
-                                //System.out.println("side1123");
-                                throw new WrongLocation("Error! You placed it too close to another one. Try again:");
-                            }
-                        } // check side 1 close to other ship
-                        if (j > 1) {
-                            if (grids[i][j - 1].equals("O ")) {
-                                //System.out.println("soide456");
-                                throw new WrongLocation("Error! You placed it too close to another one. Try again:");
-                            }
-                        } // check side 2 close to other ship
-
-                        if (grids[i][j].equals("O ")) {
-                                throw new WrongLocation("Error! A ship cannot cross other");
-                            } else {
-                                grids[i][j] = "O ";
-                            }
-                    }
+            for (int i = fromY; i <= toY ; i++) {
+                // check the ship cross another ship or not, if not place the ship
+                if (grids[i][xUnchanged].equals("O ")) {
+                    throw new WrongLocation("Error! You place the ship cross another one. Try again: ");
+                } else {
+                    grids[i][xUnchanged] = "O ";
                 }
 
+                
+                // check the west side along the ship whether it close to another ship or not
+                if (hasWestSide && grids[i][xUnchanged - 1].equals("O ")) {
+                    throw new WrongLocation("Error! You place the ship too close to another one. Try again: ");
+                }
+
+                // check the east side along the ship whether it close to another ship or not
+                if (hasEastSide && grids[i][xUnchanged + 1].equals("O ")) {
+                    throw new WrongLocation("Error! You place the ship too close to another one. Try again: ");
+                }
             }
         }
+
+
     }
 
     public static void printGrids(String[][] grids) {
@@ -221,4 +250,3 @@ class WrongLocation extends Exception {
         super(msg);
     }
 }
-
