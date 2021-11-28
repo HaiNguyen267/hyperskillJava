fcdd18c75682bfaa111ac4c20fadc99a0c46ef5d
HaiNguyen267 <88306303+HaiNguyen267@users.noreply.github.com>
2021-11-28 21:49:26 +0700

    Create stage1.java

---
+++ Readability Score/stage1.java
@@ -0,0 +1,14 @@
+package flashcards;
+
+public class Main {
+    public static void main(String[] args) {
+        String card = "1010";
+        String definition = "10";
+        
+        System.out.println("Card: ");
+        System.out.println(card);
+        System.out.println("Definition: ");
+        System.out.println(definition);
+        
+    }
+}
